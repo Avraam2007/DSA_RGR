6f08662ac2f1a4936e30220ed21effecd6e85389
AndriiAvramenk <72814303+AndriiAvramenk@users.noreply.github.com>
2026-04-05 21:10:41 +0300

    Added Excel table

--- Hanoi/Program.cs
+++ Hanoi/Program.cs
@@ -2,16 +2,43 @@
 
 namespace Hanoi {
     internal class Program {
-        static void Main(string[] args) {
-            byte disksAmount = 4;
-            byte from = 1;
-            byte to = 2;
-            byte buffer = 3;
-            HanoiTower tower = new HanoiTower();
+        public static void HanoiTowers(int disksAmount, int from, int to, int buffer) {
+            // Якщо кількість дисків у вежі менша за 0
+            if (disksAmount < 0) {
+                throw new ArgumentOutOfRangeException("Disks amount should be non-negative number!");
+            }
+            // Якщо положення початкової вежі виходить за межі допустимих значень [1,3]
+            else if (from <= 0 || from >= 4) {
+                throw new ArgumentOutOfRangeException($"This task has only 3 poles! Problem with the position of \"{nameof(from)}\".");
+            }
+            // Якщо положення кінцевої вежі виходить за межі допустимих значень [1,3]
+            else if (to <= 0 || to >= 4) {
+                throw new ArgumentOutOfRangeException($"This task has only 3 poles! Problem with the position of \"{nameof(to)}\".");
+            }
+            // Якщо положення буферної вежі виходить за межі допустимих значень [1,3]
+            else if (buffer <= 0 || buffer >= 4) {
+                throw new ArgumentOutOfRangeException($"This task has only 3 poles! Problem with the position of \"{nameof(buffer)}\".");
+            }
 
-            tower.HanoiTowers(disksAmount, from, to, buffer);
+            if (disksAmount < 0) {
+                if (disksAmount == 1) {
+                    Console.WriteLine($"Moving disk number {disksAmount} from {from} to {to}");
+                    return;
+                }
+                // Переміщуємо диск з початкової вежі до буферної
+                HanoiTowers(disksAmount - 1, from, buffer, to);
+                Console.WriteLine($"Moving disk number {disksAmount} from {from} to {to}");
+                // Переміщуємо диск з буферної вежі до кінцевої
+                HanoiTowers(disksAmount - 1, buffer, to, from);
+            }
+        }
+        public static void Main(string[] args) {
+            int disksAmount = 4;
+            int from = 1;
+            int to = 2;
+            int buffer = 3;
 
-
+            HanoiTowers(disksAmount, from, to, buffer);
         }
     }
 }
